ca306178fdc0df896a47031060172562419152e3
Rahul Khandelwal <mrkhandelwal19@gmail.com>
2023-01-15 14:37:35 +0530

    refactor: code

---
+++ Ch1-Inception/App.js
@@ -0,0 +1,30 @@
+const heading = React.createElement(
+  "h1", 
+  {
+    id: "title"
+  }, 
+  "Namaste Everyone"
+);
+
+const heading2 = React.createElement(
+  "h2", 
+  {
+    id: "title"
+  }, 
+  "This is Rahul"
+);
+
+const container = React.createElement(
+  "div",
+  {
+    id: "container"
+  },
+  [heading, heading2]
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// passing a react element inside the root
+
+//async defer
+root.render(container);
