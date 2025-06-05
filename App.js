89e1bcbb99bd78c76068b147cba1ec0b2cb55c32
k-rahul21 <mrkhandelwal19@gmail.com>
2025-06-05 18:33:28 +0530

    setup: react and react-dom packages into project

--- App.js
+++ App.js
@@ -1,43 +1,13 @@
-// const heading = React.createElement("h1", {id: "main-heading"}, "Hello React!");
+import React from "react";
+import ReactDOM from 'react-dom/client'
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-
-/** 
- * Nested Elements
- * 
- * <div id="parent">
- *  <div id="child">
- *    <h1 id="heading">Hello from Nested Container</h1>
- *  </div>
- * </div>
- * 
- */
-
-const nestedElement = React.createElement("div", {id: "parent"}, 
-  React.createElement("div", {id: "child"}, 
-    React.createElement("h1", {id: "heading"}, "Hello From Nested Container")
-  )
-);
-// root.render(nestedElement);
-
-/** 
- * Nested Elements with siblings
- * 
- * <div id="parent">
- *  <div id="child">
- *    <h1 id="heading">Hello from Nested Container</h1>
- *    <h1 id="sibling">Hello, I'm Sibling</h1>
- *  </div>
- * </div>
- * 
- */
-
-
 const nestedElementWithSiblings = React.createElement("div", {id: "parent"},
   React.createElement("div", {id: "child"}, 
     [
       React.createElement("h1", {id: "heading"}, "Hello from Nested Container"),
-      React.createElement("h1", {id: "sibling"}, "Hello, I'm Sibling")
+      React.createElement("h1", {id: "sibling"}, "Hello, I'm Siblingg")
     ]
   )
 )
