85a4cf751c28cb0e285c0697807a91c29b48852a
k-rahul21 <mrkhandelwal19@gmail.com>
2025-06-07 21:58:51 +0530

    add: header in the app

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import React from "react";
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 
-const Heading = () => (<h1>Hello From Functional Component</h1>)
+const Header = () => (
+  <div className="header-section">
+    <div className="logo">Foody Moody</div>
+    <div className="nav-items">
+      <ul>
+        <li>About Us</li>
+        <li>Cart</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  </div>
+)
+
+const AppLayout = () => {
+  return (
+    <Header/>
+  )
+}
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
 
-root.render(<Heading/>);
+root.render(<AppLayout/>);
 
