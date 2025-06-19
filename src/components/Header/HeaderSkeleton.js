ccd114dda1ada6ae1fc8305d4e6543466706147e
k-rahul21 <mrkhandelwal19@gmail.com>
2025-06-19 06:51:18 +0530

    add: skeleton for header and quick filter section

---
+++ src/components/Header/HeaderSkeleton.js
@@ -0,0 +1,7 @@
+import "./Header.css";
+
+const HeaderSkeleton = () => {
+  return <div className="header-line shine"></div>
+}
+
+export default HeaderSkeleton;
--- src/components/SkeletonListing/SkeletonListing.js
+++ src/components/SkeletonListing/SkeletonListing.js
@@ -1,14 +1,27 @@
 import CardSkeleton from "../CardSkeleton/CardSkeleton";
+import HeaderSkeleton from "../Header/HeaderSkeleton";
+import QuickFilterSkeleton from "../QuickFilters/QuickFilterSkeleton";
 
 const SkeletonListing = () => {
   return (
-    <div className="flex flex-wrap mt-5 gap-5 ml-5">
+    <div className="ml-5">
+      <HeaderSkeleton/>
+      <div className="flex mt-5 gap-2">
+        {Array(6)
+        .fill(0)
+        .map((e, index) => (
+          <QuickFilterSkeleton key={index}/>
+        ))}
+      </div>
+      <div className="flex flex-wrap mt-5 gap-5">
       {Array(15)
         .fill(0)
         .map((e, index) => (
           <CardSkeleton key={index} />
         ))}
     </div>
+    </div>
+    
   );
 };
 
