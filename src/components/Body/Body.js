b73eafa1774231e9e00f70703f394147ddeeebe8
k-rahul21 <mrkhandelwal19@gmail.com>
2025-07-07 09:19:55 +0530

    optimize: made a custom hook to get restaurant lists

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,50 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import RestaurantCard from "../RestaurantCard/RestaurantCard";
-import { restaurantsList, GET_RESTAURANTS_API } from "../../utils/constants";
+import { restaurantsList} from "../../utils/constants";
 import SearchBar from "../SearchBar/SearchBar";
 import QuickFilters from "../QuickFilters/QuickFilters";
 import SkeletonListing from "../SkeletonListing/SkeletonListing";
 import { Link } from "react-router"
+import useRestaurantList from "../../utils/useRestaurantList";
 
 const Body = () => {
-const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const {filteredRestaurants, setFilteredRestaurants} = useRestaurantList();
 
-useEffect(() => {
-  async function fetchData () {
-    try {
-      const response = await fetch(GET_RESTAURANTS_API);
-      const data = await response.json();
-      setFilteredRestaurants(data?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-    } catch (error) {
-      console.log("Error")
-    }
-  }
-  fetchData();
-},[])
 
-console.log("filteredRestaurants", filteredRestaurants);
-
-return filteredRestaurants?.length === 0 ? 
-<SkeletonListing/> : (
-  <div className="body-section">
-    {/* <SearchBar 
-      restaurantsList={restaurantsList}
-      setFilteredRestaurants={setFilteredRestaurants}
-    /> */}
-    <h1 className="mt-10 mb-2 text-2xl font-semibold">Restaurants with online food delivery in Delhi</h1>
-    <QuickFilters 
-      restaurantsList={restaurantsList}
-      setFilteredRestaurants={setFilteredRestaurants}
-    />
-    <div className="restaurant-list gap-2">{filteredRestaurants?.map((res) => (
-      <Link to={`restaurant/${res?.info.id}`} key={res?.info?.id}>
-        <RestaurantCard {...res?.info} />
-      </Link>
-    
-  ))}</div>
-  </div>
-  
-)
+  return filteredRestaurants?.length === 0 || filteredRestaurants === null ? 
+    <SkeletonListing/> : (
+    <div className="body-section">
+      {/* <SearchBar 
+        restaurantsList={restaurantsList}
+        setFilteredRestaurants={setFilteredRestaurants}
+      /> */}
+      <h1 className="mt-10 mb-2 text-2xl font-semibold">Restaurants with online food delivery in Delhi</h1>
+      <QuickFilters 
+        // restaurantsList={restaurantsList}
+        restaurantsList={filteredRestaurants}
+        setFilteredRestaurants={setFilteredRestaurants}
+      />
+      <div className="restaurant-list gap-2">{filteredRestaurants?.map((res) => (
+        <Link to={`restaurant/${res?.info.id}`} key={res?.info?.id}>
+          <RestaurantCard {...res?.info} />
+        </Link>
+      ))}</div>
+    </div>
+  )
 }
 
 export default Body;
